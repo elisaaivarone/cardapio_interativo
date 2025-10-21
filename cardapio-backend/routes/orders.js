02b09422f0467dfee9490a552834503dad66459e
Oswaldo Aivarone Motta Neto <oswaldo.aivarone@totvs.com.br>
2025-10-21 01:39:11 -0300

    feat(orders): implement routes to get orders by status and update order status

--- cardapio-backend/routes/orders.js
+++ cardapio-backend/routes/orders.js
@@ -26,4 +26,50 @@ router.post('/', async (req, res) => {
     }
 });
 
+// Rota para obter pedidos com filtro por status    
+router.get('/', async (req, res) => {
+    try {
+        const { status } = req.query;
+        let filter = {};
+
+        if (!['pending', 'ready', 'delivered'].includes(status)) {
+            return res.status(400).json({ error: 'Status inválido.' });
+    }
+    // .populate('waiterId', 'name') substitui o ID do garçom pelo nome dele
+    // .sort({ createdAt: 1 }) ordena os pedidos do mais antigo para o mais novo
+        filter = { status };
+        const orders = await Order.find(filter).populate('waiterId', 'name email').sort({ createdAt: -1 });
+        res.json(orders);
+    } catch (error) {
+        console.error('Erro ao buscar pedidos:', error);
+        res.status(500).json({ error: 'Erro ao buscar pedidos.', error: error.message });
+    }
+});
+
+// Rota para atualizar o status de um pedido
+router.patch('/:id/', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { status } = req.body;    
+
+        if (!status || !['pending', 'ready', 'delivered'].includes(status)) {
+            return res.status(400).json({ error: 'Status inválido.' });
+        }
+        const updatedOrder = await Order.findByIdAndUpdate(
+            id, 
+            { status },
+            { new: true } // Retorna o documento atualizado
+        ).populate('waiterId', 'name email');
+
+        if(!updatedOrder) {
+            return res.status(404).json({ error: 'Pedido não encontrado.' });
+        }
+        res.json(updatedOrder);
+
+    } catch (error) {
+        console.error('Erro ao atualizar pedido:', error);
+        res.status(500).json({ error: 'Erro ao atualizar pedido.', error: error.message });
+    }
+});
+
 module.exports = router;
